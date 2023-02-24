cba78734075110171fcab99443b5ec11fe227885
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-02-24 21:33:46 +0900

    [Bronze V] Title: A+B - 8, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,9 @@
+import sys
+
+T = int(input())
+
+for i in range(1,T+1) :
+    a, b = map(int, sys.stdin.readline().split())
+    c = a + b
+    print("Case #", i , ": " , sep="", end="")
+    print(a, "+", b, "=", c)

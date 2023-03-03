04505953734fda67be03ead16d316e3bdd8813bc
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-03-03 20:45:25 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,8 +1,10 @@
-dic = {'A':3 ,'B':3, 'C' :3, 'D': 4, 'E':4, 'F':4, 'G':5, 'H':5, 'I':5 , 'J':6 ,'K':6,'L':6, 'M':7,'N':7,'O':7, 'P':8,'Q':8,'R':8,'S':8, 'T':9,'U':9,'V':9, 'W':10,'X':10,'Y':10,'Z':10}
-
-alpha = list(input())
 time = 0
+alpha = ["ABC", "DEF", "GHI", "JKL", "MNO","PQRS","TUV", "WXYZ"]
+list_str = input()
 
-for i in range(len(alpha)) :
-    time += dic[alpha[i]]
+for i in list_str :
+    for j in alpha :
+        if i in list(j) :
+            time += alpha.index(j) + 3
+
 print(time)

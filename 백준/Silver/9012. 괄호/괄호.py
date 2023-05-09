605b712c94bbd07b8e6875d7faccf827e3f84066
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2023-05-09 22:40:10 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+for i in range(N):
+    PS = input()
+    stack = list(PS)
+    if stack.count("(") == stack.count(")") :
+        while len(stack) != 0 :
+            index_close = stack.index(")")
+            index_open = stack.index("(")
+            if index_open < index_close :
+                del stack[index_open]
+                del stack[index_close - 1]
+                if len(stack) == 0 :
+                    print("YES")
+                    break
+            else :
+                print("NO")
+                break
+        
+    else :
+        print("NO")
